4b1f7cdfd90aabbf5d494c9bbf240d3e917b3b55
Jimmy <mutabazijimmy9@gmail.com>
2022-06-23 15:29:03 +0200

    my javascript introduction project that prints my profile

---
+++ java.js
@@ -0,0 +1,7 @@
+let myName = "Jimmy MUTABAZI";
+let myCountry = "Rwanda";
+let myHeight = 1.71;
+
+console.log("My name is " + myName);
+console.log("My Country is " + myCountry);
+console.log("My height is " + myHeight + "m");
